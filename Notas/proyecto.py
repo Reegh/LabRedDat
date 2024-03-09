50187cd3fb08a48a9dba7487e3a446c7d81d557a
Giovanna <gioreneeha@gmail.com>
2024-03-09 01:55:14 -0600

    distintos fits

--- Notas/proyecto.py
+++ Notas/proyecto.py
@@ -6,19 +6,20 @@ from scipy.optimize import curve_fit
 import streamlit as st
 import matplotlib.pyplot as plt
 import math
+from scipy.special import comb
 
 print('nuevo')
-def fit_function(x, n, p):
-    return stats.binom.pmf(x, n, p)
+def fit_func(x, n, p):
+    return stats.binom(x, n, p)
 
-# def fit_function(x,n,p):
-#     # Todo lo que este aqui adentro es parte de lo que se ejecuta en la funcion
-
-#     comb = math.comb(n,x)
-#     p_x = p**x
-#     q_nx = (1-p)**(n-x)
-
-#     return comb*p_x*q_nx
+def fit_function(x,n,p):
+# Todo lo que este aqui adentro es parte de lo que se ejecuta en la funcion
+    n = int(n)
+    x = np.array(x, dtype=int)
+    combi = comb(n,x)
+    px = p**x
+    qnx = (1-p)**(n-x)
+    return combi*px*qnx
 
 data = pd.read_csv('Binomial-fichas.csv')
 print(data)
@@ -29,30 +30,35 @@ group = df['GM'].value_counts().reindex(value_range, fill_value=0).reset_index()
 print('pandas')
 print(group)
 group_2 = pd.Series.to_numpy(df['GM'].value_counts().reindex(value_range, fill_value=0))
+print(group_2)
 binomial_2 = px.bar(x=group['GM'], y=group['count'])
 binomial = px.scatter(x=group['GM'], y=group['count'], trendline="rolling", trendline_options=dict(window=1, win_type='gaussian', function_args=dict(std=2)))
 binomial.add_bar(x=group['GM'], y=group['count'], marker_color='#1e6905', name='binomial')
 st.plotly_chart(binomial_2)
 st.plotly_chart(binomial)
 
-df = px.data.stocks(datetimes=True)
-fig = px.scatter(df, x="date", y="GOOG", trendline="rolling", trendline_options=dict(window=5),
-                title="5-point moving average")
-st.plotly_chart(fig)
-
-p0=[1,1/2]
-res, cov = curve_fit(fit_function, value_range, group_2, [5,1])
+p0=[10,1/2]
+res, cov = curve_fit(fit_function, value_range, group_2, p0=p0)
 print(res)
 print(cov)
 
 fig, ax = plt.subplots()
 ax.bar(value_range, group_2)
-ax.plot(value_range, fit_function(value_range, *res), color='C1')
+ax.plot(value_range, fit_function(value_range, *res)*105, color='C3')
 
-# rng = np.random.default_rng()
-# dist = stats.nbinom
-# shapes = (5, 0.5)
-# bounds = [(0, 26), (0, 1)]
-# res = stats.fit(dist, group_2, bounds)
-# res.plot()
+
+temp = df['GM']
+data_sort = temp.sort_values()
+normfit = stats.norm.fit(temp)
+print(normfit)
+# plt.figure(figsize=(2,2))
+plt.hist(temp)
+plt.plot(data_sort, stats.norm.pdf(data_sort, *normfit)*98)
+plt.show()
+
+# dist = stats.norm
+# bounds = [(0, 25), (1, 25)]
+# res_2 = stats.fit(dist, temp, bounds)
+# print(res_2)
+# res_2.plot()
 st.pyplot(plt.show())
